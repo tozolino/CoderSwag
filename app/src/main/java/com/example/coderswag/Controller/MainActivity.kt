67d2289e920bcd489460a81d3675c9a540a1b83a
Svetozar Pavlovic <s.pavlovic@levi9.com>
2020-04-18 13:18:43 +0200

    feat: Add ViewHolder

--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.coderswag.Adapter.CategoryAdapter
 import com.example.coderswag.Model.Category
 import com.example.coderswag.R
 import com.example.coderswag.Services.DataService
@@ -11,15 +12,14 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
+        adapter = CategoryAdapter(this,
             DataService.categories)
 
         lvCategories.adapter = adapter
